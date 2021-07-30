d75ff7702e4cebbd4fa89a21d6463a81a8fd3a98
Dongyang Yan <2597034Y@student.gla.ac.uk>
2021-07-30 13:46:10 +0100

    ch9 exercise

--- rango/forms.py
+++ rango/forms.py
@@ -4,8 +4,10 @@ from rango.models import Page, Category, UserProfile
 
 # We could add these forms to views.py, but it makes sense to split them off into their own file.
 
+
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
+    name = forms.CharField(max_length=Category.NAME_MAX_LENGTH,
+                           help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -14,15 +16,18 @@ class CategoryForm(forms.ModelForm):
         model = Category
         fields = ('name',)
 
+
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH, help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH,
+                            help_text="Please enter the title of the page.")
+    url = forms.URLField(
+        max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
         model = Page
         exclude = ('category',)
-    
+
     def clean(self):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
@@ -30,8 +35,9 @@ class PageForm(forms.ModelForm):
         if url and not url.startswith('http://'):
             url = f'http://{url}'
             cleaned_data['url'] = url
-        
+
         return cleaned_data
+
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -39,6 +45,7 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password',)
+
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
